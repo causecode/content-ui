eb2ca32c9e9c3650c12f021547bb1a9b664cb080
Hardik Modha <hardik.modha@causecode.com>
2016-11-23 10:03:01 +0530

    Few changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,30 @@
+var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var isProduction = process.argv.indexOf('--production') != -1;
+
+if (isProduction) {
+    // Adding Production environment specific features.
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({  // Used for minification of .js and .css files.
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    );
+} else {
+    new webpack.DefinePlugin({
+        'process.env': {
+            'NODE_ENV': JSON.stringify('development')
+        }
+    })
+}
 
 module.exports = {
     entry: './src/index.tsx',
@@ -16,7 +41,7 @@ module.exports = {
             {test: /\.tsx?$/, loader: 'tslint', exclude: /node_modules/}
         ],
         loaders: [
-            {test: /\.tsx?$/, exclude: /node_modules/, loaders: ['ts-loader']},
+            {test: /\.tsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'ts-loader']},
             {test: /\.css$/, loader: 'style!css'},
             {test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'},
             {test: /\.(jpg|jpeg|gif|png)$/, loader: 'url-loader?limit=10&mimetype=image/(jpg|jpeg|gif|png)&name=images/[name].[ext]'},
